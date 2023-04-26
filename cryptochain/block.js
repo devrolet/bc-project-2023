3e5cb1b6c96e803dfecd83276704dda71f3666ca
Chet Hill <chetdhill@gmail.com>
2023-04-26 15:00:21 -0500

    Difficulty and Nonce Value: Passing Tests

--- cryptochain/block.js
+++ cryptochain/block.js
@@ -2,11 +2,13 @@ const { GENESIS_DATA } = require('./config');
 const cryptoHash = require('./crypto-hash');
 class Block {
   // Using an object in case block data is not organized by params
-  constructor({ timestamp, lastHash, hash, data }) {
+  constructor({ timestamp, lastHash, hash, data, nonce, difficulty }) {
     this.timestamp = timestamp;
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+    this.nonce = nonce;
+    this.difficulty = difficulty;
   }
 
   // Create Genesis Block Factory Method
@@ -16,14 +18,25 @@ class Block {
 
   // Mine Block Method
   static mineBlock({ lastBlock, data }) {
-    const timestamp = Date.now();
+    let hash, timestamp;
+    // const timestamp = Date.now();
     const lastHash = lastBlock.hash;
+    const { difficulty } = lastBlock;
+    let nonce = 0;
+
+    do {
+      nonce++;
+      timestamp = Date.now();
+      hash = cryptoHash(timestamp, lastHash, data, nonce, difficulty);
+    } while (hash.substring(0, difficulty) !== '0'.repeat(difficulty));
 
     return new this({
       timestamp,
       lastHash,
       data,
-      hash: cryptoHash(timestamp, lastHash, data)
+      difficulty,
+      nonce,
+      hash
     });
   }
 
