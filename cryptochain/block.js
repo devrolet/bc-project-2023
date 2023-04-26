f845579a53c999b81c76bc7d2bca2230d3854bd2
Chet Hill <chetdhill@gmail.com>
2023-04-26 09:51:36 -0500

    Create Genesis Block: Tests Passing

--- cryptochain/block.js
+++ cryptochain/block.js
@@ -1,3 +1,4 @@
+const { GENESIS_DATA } = require('./config');
 class Block {
   // Using an object in case block data is not organized by params
   constructor({ timestamp, lastHash, hash, data }) {
@@ -5,6 +6,11 @@ class Block {
     this.lastHash = lastHash;
     this.hash = hash;
     this.data = data;
+  }
+
+  // Create Genesis Block Factory Method
+  static genesis() {
+    return new this(GENESIS_DATA);
   }
 }
 
