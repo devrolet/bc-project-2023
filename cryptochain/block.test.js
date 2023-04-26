dc7bebb4670a92156b8aaf237e7e49701480cfdb
Chet Hill <chetdhill@gmail.com>
2023-04-26 15:12:56 -0500

    Make Difficulty and Mine Rate Dynamic: Tests

--- cryptochain/block.test.js
+++ cryptochain/block.test.js
@@ -1,10 +1,10 @@
 const Block = require('./block');
-const { GENESIS_DATA } = require('./config');
+const { GENESIS_DATA, MINE_RATE } = require('./config');
 const cryptoHash = require('./crypto-hash');
 
 
 describe('Block', () => {
-  const timestamp = 'a-date'; 
+  const timestamp = 2000; 
   const lastHash = 'foo-hash'; 
   const hash = 'bar-hash';
   const data = ['blockchain', 'sample-data']; 
@@ -75,4 +75,20 @@ describe('Block', () => {
     });
 
   });
+
+  describe('adjustDifficulty()', () => {
+    it('raises the difficulty for a quickly mined block', () => {
+      expect(Block.adjustDifficulty({ 
+        originalBlock: block, timestamp: block.timestamp + MINE_RATE - 100
+      })).toEqual(block.difficulty+1);
+    });
+
+    it('lowers the difficulty for a slowly mined block', () => {
+      expect(Block.adjustDifficulty({
+        originalBlock: block, timestamp: block.timestamp + MINE_RATE + 100
+      })).toEqual(block.difficulty-1);
+    });
+
+  });
+
 });
