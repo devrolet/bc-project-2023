ed207152fe026a74b7109134cf90c756dbfe9e28
Chet Hill <chetdhill@gmail.com>
2023-04-27 09:11:21 -0500

    POST /api/mine, GET /api/blocks endpoints

--- cryptochain/index.js
+++ cryptochain/index.js
@@ -1,12 +1,31 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const Blockchain = require('./blockchain');
+
 
 const app = express();
 const blockchain = new Blockchain();
 const PORT = 3000;
 
+/* 
+  MIDDLEWARE
+*/
+
+app.use(bodyParser.json());
+
+/*
+  API ENDPOINTS
+*/
+
 app.get('/api/blocks', (req, res) => {
   res.json(blockchain.chain);
+});
+
+app.post('/api/mine', (req, res) => {
+  const { data } = req.body;
+
+  blockchain.addBlock({ data });
+  res.redirect('/api/blocks');
 });
 
 app.listen(PORT, () => console.log(`Listening at localhost: ${PORT}`));
