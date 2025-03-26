ac5821f7d0c33a340fd5f0c701e5593ae7e265aa
Chet Hill <chetdhill@gmail.com>
2025-03-26 16:57:27 -0500

    Update Transaction in the API

--- cryptochain/wallet/transaction-pool.js
+++ cryptochain/wallet/transaction-pool.js
@@ -6,6 +6,12 @@ class TransactionPool {
     setTransaction(transaction) {
         this.transactionMap[transaction.id] = transaction;
     }
+
+    existingTransaction({ inputAddress }) {
+        const transacions = Object.values(this.transactionMap);
+        
+        return transacions.find(transacion => transacion.input.address === inputAddress);
+    }
 }
 
 module.exports = TransactionPool;
