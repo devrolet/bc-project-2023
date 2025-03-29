594cf7601744a968d2373e4dfb3c978b84258e42
Chet Hill <chetdhill@gmail.com>
2025-03-29 16:57:54 -0500

    Poll the Transaction Pool

--- cryptochain/client/src/components/TransactionPool.js
+++ cryptochain/client/src/components/TransactionPool.js
@@ -1,18 +1,30 @@
 import React, { Component } from "react";
+import { Button } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import Transaction from "./Transaction";
 
+const POLL_INTERVAL_MS = 10000;
+
 class TransactionPool extends Component {
-    state = { tranactionPoolMap: {}};
+    state = { transactionPoolMap: {} };
 
     fetchTransactionPoolMap = () => {
-        fetch('http://localhost:3000/api/transaction-pool-map')
+        fetch(`${document.location.origin}/api/transaction-pool-map`)
             .then(response => response.json())
-            .then(json => this.setState({ tranactionPoolMap: json }));
+            .then(json => this.setState({ transactionPoolMap: json }));
     }
 
     componentDidMount() {
         this.fetchTransactionPoolMap();
+
+        this.fetchPoolMapInterval = setInterval(
+            () => this.fetchTransactionPoolMap(),
+            POLL_INTERVAL_MS
+        );
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.fetchPoolMapInterval);
     }
 
     render() {
@@ -21,7 +33,7 @@ class TransactionPool extends Component {
                 <div><Link to='/'>Home</Link></div>
                 <h3>Transaction Pool</h3>
                 {
-                    Object.values(this.state.tranactionPoolMap).map(transaction => {
+                    Object.values(this.state.transactionPoolMap).map(transaction => {
                         return(
                             <div key={transaction.id}>
                                 <hr />
